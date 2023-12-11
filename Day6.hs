4a45f9956f7fb9a68e52bdf39628fdd4cec415b2
Kyle Mayes <kyle@mayeses.com>
2023-12-11 03:25:07 -0500

    Solve day 6

--- Day6.hs
+++ Day6.hs
@@ -2,15 +2,26 @@ module Day6 (day6) where
 
 import qualified Data.Text as T
 
-type Input = [T.Text]
+data Race = Race {raceTime :: Int, raceDistance :: Int} deriving (Show)
+
+type Input = [Race]
 
 parse :: T.Text -> Input
-parse = filter (not . T.null) . T.lines
+parse text =
+  let [times, distances] = T.lines text
+      numbers = map (read . T.unpack) . drop 1 . T.words
+   in zipWith Race (numbers times) (numbers distances)
+
+raceWins :: Race -> [Int]
+raceWins (Race time distance) = filter (> distance) [x * (time - x) | x <- [0 .. time]]
 
 part1 :: Input -> Int
-part1 _ = 0
+part1 = product . map (length . raceWins)
 
 part2 :: Input -> Int
-part2 _ = 0
+part2 races =
+  let time = read $ concatMap (show . raceTime) races
+      distance = read $ concatMap (show . raceDistance) races
+   in length . raceWins $ Race time distance
 
 day6 = (parse, part1, part2)
